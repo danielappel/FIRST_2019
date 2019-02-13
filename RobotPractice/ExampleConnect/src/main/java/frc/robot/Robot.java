1d2d9f50b54cc9f704261de92386e9135e287c4f
Erik Voss <erik66voss@gmail.com>
2019-02-13 09:21:50 -0800

    Updated and Hopefully Fixed
    Hopefully all the code is fixed now
    -Erik Voss

--- RobotPractice/ExampleConnect/src/main/java/frc/robot/Robot.java
+++ RobotPractice/ExampleConnect/src/main/java/frc/robot/Robot.java
@@ -40,6 +40,8 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 import java.lang.Math;
+
+import edu.wpi.first.wpilibj.command.TimedCommand;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
@@ -88,7 +90,7 @@ public class Robot extends TimedRobot{
   private double speedMultiplier = 2.0;
   private int numberOfButton11Presses = 0;
   private double speedAdder = 0.0;
-  private final double accelerateIncrement = 0.0;
+  private final double accelerateIncrement = 0.05;
   private int numberOfRightBumperPresses = 0;
   private int numberOfLeftBumperPresses = 0;
 
@@ -108,8 +110,8 @@ public class Robot extends TimedRobot{
   static double lengthBetweenContours;
 
   // Wait Command Declarations
-  private WaitCommand hatchWait = new WaitCommand(2);
-  private WaitCommand liftWait = new WaitCommand(2);
+  private TimedCommand hatchTimer = new TimedCommand(.5);
+  private TimedCommand liftTimer = new TimedCommand(.5);
 
   /**
    * This function is run when the robot is first started up and should be
@@ -181,13 +183,14 @@ public class Robot extends TimedRobot{
    */
   @Override
   public void teleopPeriodic() {
+    
     robotMovement();
     cameraMovement();
     liftMotorControl();
     hatchMotorControl();
-    testLinearActuator();
-
+    //testLinearActuator();
     //breakInMotors();
+
   }
 
   /**
@@ -198,92 +201,130 @@ public class Robot extends TimedRobot{
   }
 
   public void testLinearActuator(){
+
     if(controller.getRawButton(5)){
       linearAcServo.set(1);
     }
-    if(controller.getRawButton(6)){
+    else if(controller.getRawButton(6)){
       linearAcServo.set(0.2);
     }
+
   }
   
   public void robotMovement(){
-    /*
-    * when trigger is pressed full speed is enabled (this can be mapped to any button)
-    * when trigger is released robot moves at half speed
-    */
-      //jay is an epic gamer and this was his idea 
+
       if(button3.get()){
         //hold button 3 to keep robot slow
         speedMultiplier = 2.5;
       }
+
       else if(button4.get()){
         //hold button 4 to speed up "gradually"
-        if(-1*controller.getY()>0 && speedAdder < .5) //safety
+        if(controller.getY() > 0 && speedAdder < .5){   //safety
           speedAdder += accelerateIncrement;
-        else if(-1*controller.getY()<0 && speedAdder > -.5){
+        } 
+
+        else if(controller.getY() < 0 && speedAdder > -.5){    // safety
           speedAdder -= accelerateIncrement;
         }
+
       }
+
       else{
         //default speed
-        if(speedAdder > 0)
+        if(speedAdder > 0){
           speedAdder -= accelerateIncrement;
-        if(speedAdder < 0){
+        }
+
+        else if(speedAdder < 0){
           speedAdder += accelerateIncrement;
         }
+
         speedMultiplier = 2;
       }
+
       myDrive.arcadeDrive(-1 * controller.getY()/speedMultiplier + speedAdder, controller.getAxis(Joystick.AxisType.kTwist)/speedMultiplier);
-      System.out.println(controller.getY()/speedMultiplier+speedAdder);  
+
   }
 
 
   public void liftMotorControl(){  
     
-    if(controller2.getAButton()){
-      motor_lift.setSpeed(.5);
-    }
-    else if(controller2.getBButton()){
-      motor_lift.setSpeed(-.5);
+    if(controller2.getBumperPressed(Hand.kRight)){
+      liftTimer.start();
+      if(liftTimer.isRunning()){
+        motor_lift.setSpeed(.4);
+      }
+      else if(liftTimer.isCompleted()){
+        motor_lift.stopMotor();
+      }
     }
 
-    if(controller2.getXButton()){
-      motor_lift.setSpeed(0);
+    else if(controller2.getBumperPressed(Hand.kLeft)){
+      liftTimer.start();
+      if(liftTimer.isRunning()){
+        motor_lift.setSpeed(-.4);
+      }
+      else if(liftTimer.isCompleted()){
+        motor_lift.stopMotor();
+      }
     }
 
   }
 
   public void hatchMotorControl(){
-    if(controller2.getBumperPressed(Hand.kRight)){
-      numberOfRightBumperPresses += 1;
+
+    if(controller2.getTriggerAxis(Hand.kRight) > 0){
+      hatchTimer.start();
+      if(hatchTimer.isRunning()){
+        motor_panelGrabber.setSpeed(.5);
+      }
+      else if(hatchTimer.isCompleted()){
+        motor_panelGrabber.stopMotor();
+      }
+    }
+      
+    else if(controller2.getTriggerAxis(Hand.kLeft) > 0){
+      hatchTimer.start();
+      if(hatchTimer.isRunning()){
+        motor_panelGrabber.setSpeed(-.5);
+      }
+      else if(hatchTimer.isCompleted()){
+        motor_panelGrabber.stopMotor();
+      }   
     }
 
-    if(controller2.getBumperPressed(Hand.kRight)){
-    }
-    else if(controller2.getBumperPressed(Hand.kLeft)){
-    }
-    if(controller2.getBButton()){
-      motor_panelGrabber.setSpeed(0);
-    }
   }
 
 
   public void moveCameraX(int speed){
+
     cameraServoX.setAngle(cameraServoX.getAngle()+speed);
+
   }
 
+
   public void moveCameraY(int speed){
+
     cameraServoY.setAngle(cameraServoY.getAngle()+speed);
+
   }
   
+
   public void centerCamera(){
+
     cameraServoX.setAngle(90);
     cameraServoY.setAngle(90);
+
   }
+
+
   public void cameraMovement(){
+
       if(controller.getRawButton(10)){
         centerCamera();
       }
+
       switch(controller.getPOV()){
         //up  
         case 0:
@@ -322,19 +363,27 @@ public class Robot extends TimedRobot{
           moveCameraY(2);
           break;
       }    
+
   }
 
-  public void breakInMotors(){
+/**
+ * public void breakInMotors(){
+
     if(controller.getRawButton(11)){
       numberOfButton11Presses++;
     }
+
     if(numberOfButton11Presses % 2 == 1){
       myDrive.arcadeDrive(.75,0);
     }
     else{
       myDrive.arcadeDrive(0, 0);
     }
+
   }
+ */
+  
+
 }
 
     
